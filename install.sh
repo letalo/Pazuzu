497dd299dc3e75493343345a5cfcdb6e8b334e3a
warenack <jpc4ar@gmail.com>
2016-01-13 04:20:50 +1030

    commit install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -n 1 -p "do you want to install Pazuzu (y/[a]): " AMSURE 
+[ "$AMSURE" = "y" ] || exit
+echo "" 1>&2
+
+echo "copying... Pazuzu-->/usr/sbin"
+cp ./Pazuzu /usr/sbin
+
+echo "go into the directory: ./daemon"
+cd ./daemon
+
+echo "copying... daemon script-->/etc/init.d"
+cp ./Pazuzu /etc/init.d
+echo "setting access rights"
+chmod +x /etc/init.d/Pazuzu
+
+echo "copying... daemon PID-->/var/run"
+cp ./Pazuzu.pid /var/run
+
+echo "copying... daemon config-->/etc/init"
+cp ./Pazuzu.conf /etc/init
+
